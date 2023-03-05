8edd8b104084f317397ec55c791b05c143c2ade3
Jason Zhu <jasonzhuyq@outlook.com>
2023-03-05 21:03:15 +1100

    Added a sample static Pokemon card

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Header } from './Header';
 import { Filters } from './Filters';
+import { Pokemon } from './Pokemon';
 
 function App() {
   return (
     <div className="App app_container">
       <Header />
       <Filters />
+      <Pokemon />
     </div>
   );
 }
