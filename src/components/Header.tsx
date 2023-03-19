bbaec4a027c23835d6c753a1e545449e3f97d7d3
Jason Zhu <jasonzhuyq@outlook.com>
2023-03-19 13:16:17 +1100

    Move Header.tsx into components directory

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../assets/poke_logo.png';
+
+const Header = () => {
+  return (
+    <div>
+      <div className="poke__logos">
+        <img src={logo} alt="Poke Logo" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
