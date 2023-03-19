a867ce00015ca1f5ac1f4783d5e4bcd9e846df88
Jason Zhu <jasonzhuyq@outlook.com>
2023-03-19 15:17:58 +1100

    Use Create-React-App absolute path feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import Header from 'components/Header';
 import { Filters } from './Filters';
 import { Pokemon } from './features/Pokedex/Pokemon';
 
