9e48382026a1aaf2e372e594f1ebd14b780f9ef5
Jason Zhu <jasonzhuyq@outlook.com>
2023-03-23 21:03:36 +1100

    Implemented jest test for Pokemon Card component

---
+++ src/features/Pokedex/Pokemon/Pokemon.test.ts
@@ -0,0 +1,15 @@
+import { formatNumber } from './Pokemon';
+
+describe('Test Functions', () => {
+  describe('formatNumber', () => {
+    it('should format single digit integer correctly', () => {
+      expect(formatNumber(6)).toBe('#006');
+    });
+    it('should format double digit integer correctly', () => {
+      expect(formatNumber(16)).toBe('#016');
+    });
+    it('should format triple digit integer correctly', () => {
+      expect(formatNumber(116)).toBe('#116');
+    });
+  });
+});
