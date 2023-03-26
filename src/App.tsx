db682dc2a246cd1b29f484b6ebb1baa37fc811c7
Jason Zhu <jasonzhuyq@outlook.com>
2023-03-26 15:56:51 +1100

    Move Loading screen into Pokedex.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,8 @@ import React from 'react';
 import './App.css';
 import Header from 'components/Header';
 import Pokedex from './features/Pokedex';
-import { useGetPokemonListQuery } from './features/Pokedex/pokedexApi';
-import Loading from './components/Loading';
 
 function App() {
-  const { data, error, isLoading } = useGetPokemonListQuery(100);
-  if (isLoading) {
-    return (
-      <div className="App app_container">
-        <Loading />
-      </div>
-    );
-  }
   return (
     <div className="App app_container">
       <Header />
--- src/features/Pokedex/Pokedex.tsx
+++ src/features/Pokedex/Pokedex.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import Pokemon from './Pokemon';
 import Filters from './Filters';
+import { useGetPokemonListQuery } from './pokedexApi';
+import Loading from 'components/Loading';
 
 import charizard from 'features/Pokedex/Pokemon/assets/stories/charizard.svg';
 
 const Pokedex = () => {
+  const { data, error, isLoading } = useGetPokemonListQuery(100);
+
   return (
     <>
       <Filters />
-      <Pokemon
-        name={'Charizard'}
-        number={6}
-        image={charizard}
-        types={['fire', 'flying']}
-      />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <Pokemon
+          name={'Charizard'}
+          number={6}
+          image={charizard}
+          types={['fire', 'flying']}
+        />
+      )}
     </>
   );
 };
