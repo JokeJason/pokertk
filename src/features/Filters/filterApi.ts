1055a14457149c44cbc82e5b0624665ebb7e9e43
Jason Zhu <jasonzhuyq@outlook.com>
2023-05-08 20:34:13 +1000

    Put types into correct directory (fix filterApi.ts)

--- src/features/Filters/filterApi.ts
+++ src/features/Filters/filterApi.ts
@@ -1,9 +1,6 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
 import { pokeApiBaseQuery } from '../../services/pokeapi/paginationBaseQuery';
-import {
-  RegionListResponseData,
-  TypeListResponseData,
-} from '../Pokedex/types/api';
+import { RegionListResponseData, TypeListResponseData } from './types/api';
 
 export const filterApi = createApi({
   reducerPath: 'filterApi',
